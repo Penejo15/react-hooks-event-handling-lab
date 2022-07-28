7bf06fc9abd3d3a84b4cb4f38329245ba3d9adfb
peter enejo <petstorm101@gmail.com>
2022-07-28 04:06:08 +0100

    react-hooks-event-handling

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  function hanFocus() {
+    console.log("Good!");
+  }
+
+  function hanBlur() {
+    console.log("Hey! Eyes on me!");
+  }
+
+  return (
+    <button onFocus={hanFocus} onBlur={hanBlur}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
